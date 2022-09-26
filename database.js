2bc09a7e7ff59e8867646ef8bd4c795d0321932b
EgloDevelopment <UNUstifA4W346TcF2Qwc@gmail.com>
2022-09-25 20:54:31 -0600

    bamhurger

--- database.js
+++ database.js
@@ -1,44 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require("mongoose");
-const userModel = require("./models/user");
-
-const app = express();
-const port = 3001;
-
-app.use(cors());
-
-mongoose.connect('mongodb+srv://root:Qr76sivt9K9Smv@main.a6uehiu.mongodb.net/Eglo?retryWrites=true&w=majority',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-    console.log("Connected successfully");
+var mysql = require("mysql");
+var conn = mysql.createConnection({
+  host: "localhost", // Replace with your host name
+  user: "root", // Replace with your database username
+  password: "", // Replace with your database password
+  database: "test", // // Replace with your database Name
 });
-
-// Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use('/details/:id', async (req, res) => {
-    const id = req.id;
-    const user = await db.collection("Users").findOne({address: id});
-    //const user = await db.collection("Users").userModel.findOne({name: "Company Inc"});
-    res.json(user);
+conn.connect(function (err) {
+  if (err) throw err;
+  console.log("Database is connected successfully!");
 });
-
-app.use('/login', (req, res) => {
-    //const user = await userModel.find({});
-    res.send({
-        token: 'test123'
-    });
-});
-
-
-app.listen(port, () => console.log(`Mon has been Goosed`))
+module.exports = conn;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var sanitizer = require("sanitizer");
-const path = require('path');
+const path = require("path");
 var express = require("express");
 var router = express.Router();
 var db = require("../database");
 var validate = require("../middleware/auth");
 //const passport = require("passport");
-
 
 var get_cookies = function (request) {
   var cookies = {};
@@ -22,12 +21,9 @@ router.get("", validate, function (req, res, next) {
   var sql = "SELECT * FROM users WHERE token = " + token + "";
   db.query(sql, function (err, data, fields) {
     if (err) throw err;
-    res.render("home", { Name: data[0].name, userData: data});
+    res.render("home", { Name: data[0].name, userData: data });
   });
 });
-
-
-
 
 //------------------------------------------TESTING----------------------------------------//
 
@@ -47,11 +43,11 @@ router.get("/error", function (req, res, next) {
 //-----------------------------FOR THOSE WHO THINK THEY ARE HACKERMANS---------------------------//
 
 router.get("/register/:promo", function (req, res, next) {
-  res.redirect('/users/register/' + req.params.promo);
+  res.redirect("/users/register/" + req.params.promo);
 });
 
 router.get("/login", function (req, res, next) {
-  res.redirect('/users/login');
+  res.redirect("/users/login");
 });
 
 module.exports = router;
