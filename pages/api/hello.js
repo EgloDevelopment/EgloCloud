e6c9db64ff3b6e91ff956e95127909c6af87c490
Eglo <105808341+EgloDevelopment@users.noreply.github.com>
2023-01-07 07:09:42 +0000

    Added tailwind and DasyUI, and made MongoDB work, ready for development

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import clientPromise from "../../functions/mongo";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+	const client = await clientPromise;
+	const db = client.db("Main");
+	const database_interaction = await db.collection("Users").find({}).toArray();
+	res.json({ status: 200, data: database_interaction });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+//import Sidebar from "../components/sidebar"
+
 export default function Page() {
-  return (
-    <>
-			<p>Home</p>
-    </>
-  )
+	return (
+		<>
+			<div class="grid place-items-center mt-14">
+				<div className="stats shadow">
+					<div className="stat place-items-center">
+						<div className="stat-value">Toaster</div>
+						<div className="stat-desc">cooper@eglo.pw</div>
+					</div>
+				</div>
+			</div>
+			<div className="toast toast-end">
+				<div className="alert alert-primary">
+					<div>
+						<span>Version: 0.0.1</span>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
